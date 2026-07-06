c753de6cfc649ddcfc7f2c2e464b3b606a962c6b
Espy <espimyte@gmail.com>
2026-07-05 23:47:39 -0700

    Cleanup config

--- config.js
+++ config.js
@@ -1,30 +1,14 @@
-export const OUTPUT_FOLDER = "example" // Folder where achievement data and icons are saved
+/* Folder where achievement data and icons are saved */
+export const OUTPUT_FOLDER = "example" // 
+
+/**
+ * "exclude" - default behavior, gets all owned games on your steam account and excludes anything in 'EXCLUDE_IDS'
+ * "include" - only includes games in 'INCLUDE_IDS'
+ */
 export const FETCH_MODE = "include"
 
 /* App IDs of games to exclude */
-export const EXCLUDE_IDS = [
-	239350, // Spelunky
-	238460, // BattleBlock Theater
-	286160, // Tabletop Simulator
-	311690, // Enter the Gungeon
-	367540, // Starbound - Unstable
-	387290, // Ori and the Blind Forest: Definitive Edition
-	434170, // The Jackbox Party Pack 3
-	774461, // The Jackbox Party Pack 5
-	1005300, // The Jackbox Party Pack 6
-	1211630, // The Jackbox Party Pack 7
-	1552350, // The Jackbox Party Pack 8
-	945360, // Among Us
-	960090, // Bloons TD 6
-	1041920, // If Found...
-	1172620, // Sea of Thieves
-	2338140, // Dokapon Kingdom: Connect
-	236110, // Dungeon Defenders II
-]
+export const EXCLUDE_IDS = []
 
 /* App IDs of games to include */
-export const INCLUDE_IDS = [
-	1030300, // Silksong
-	250900, // The Binding of Isaac: Rebirth
-	333640, // Caves of Qud
-]
+export const INCLUDE_IDS = []
