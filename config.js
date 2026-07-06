78e2ed6d816b6da6c598b7135fb393f5ceafc05a
Espy <espimyte@gmail.com>
2026-07-06 00:16:08 -0700

    Set config to default values

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ export const OUTPUT_FOLDER = "example" //
  * "exclude" - default behavior, gets all owned games on your steam account and excludes anything in 'EXCLUDE_IDS'
  * "include" - only includes games in 'INCLUDE_IDS'
  */
-export const FETCH_MODE = "include"
+export const FETCH_MODE = "exclude"
 
 /* App IDs of games to exclude */
 export const EXCLUDE_IDS = []
